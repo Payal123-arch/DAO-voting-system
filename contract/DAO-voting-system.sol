b08a6e4c20d1ee210ba1976f09ffbbfd4a22d912
Payal123-arch <pp804368@gmail.com>
2025-05-21 17:44:29 +0530

    Update DAO-voting-system.sol

--- contract/DAO-voting-system.sol
+++ contract/DAO-voting-system.sol
@@ -37,6 +37,7 @@ contract DAOVoting {
     event ProposalExecuted(uint256 indexed proposalId);
     event MemberAdded(address indexed member);
     event MemberRemoved(address indexed member);
+    event VotingPeriodChanged(uint256 newPeriod);
 
     modifier onlyAdmin() {
         require(msg.sender == admin, "Only admin can call this function");
@@ -55,69 +56,49 @@ contract DAOVoting {
         emit MemberAdded(msg.sender);
     }
 
-    /**
-     * @dev Create a new proposal for the DAO to vote on
-     * @param _description Description of the proposal
-     * @return proposalId The ID of the newly created proposal
-     */
     function createProposal(string memory _description) external onlyMembers returns (uint256) {
         proposalCount++;
         uint256 proposalId = proposalCount;
-        
+
         Proposal storage proposal = proposals[proposalId];
         proposal.id = proposalId;
         proposal.description = _description;
         proposal.deadline = block.timestamp + votingPeriod;
         proposal.executed = false;
-        
+
         emit ProposalCreated(proposalId, _description, proposal.deadline);
         return proposalId;
     }
 
-    /**
-     * @dev Vote on an existing proposal
-     * @param _proposalId The ID of the proposal to vote on
-     * @param _support Whether the voter supports the proposal or not
-     */
     function vote(uint256 _proposalId, bool _support) external onlyMembers {
         Proposal storage proposal = proposals[_proposalId];
-        
+
         require(block.timestamp < proposal.deadline, "Voting period has ended");
         require(!proposal.hasVoted[msg.sender], "Already voted");
-        
+
         proposal.hasVoted[msg.sender] = true;
-        
+
         if (_support) {
             proposal.forVotes += 1;
         } else {
             proposal.againstVotes += 1;
         }
-        
+
         emit Voted(_proposalId, msg.sender, _support);
     }
 
-    /**
-     * @dev Execute a proposal after voting period ends
-     * @param _proposalId The ID of the proposal to execute
-     */
     function executeProposal(uint256 _proposalId) external {
         Proposal storage proposal = proposals[_proposalId];
-        
+
         require(block.timestamp >= proposal.deadline, "Voting period not yet ended");
         require(!proposal.executed, "Proposal already executed");
         require(proposal.forVotes > proposal.againstVotes, "Proposal did not pass");
-        
+
         proposal.executed = true;
-        
+
         emit ProposalExecuted(_proposalId);
-        
-        // In a real implementation, you would execute the proposal's actions here
     }
 
-    /**
-     * @dev Add a new member to the DAO
-     * @param _member Address of the new member
-     */
     function addMember(address _member) external onlyAdmin {
         require(!members[_member], "Already a member");
         members[_member] = true;
@@ -125,10 +106,6 @@ contract DAOVoting {
         emit MemberAdded(_member);
     }
 
-    /**
-     * @dev Remove a member from the DAO
-     * @param _member Address of the member to remove
-     */
     function removeMember(address _member) external onlyAdmin {
         require(members[_member], "Not a member");
         require(_member != admin, "Cannot remove admin");
@@ -137,11 +114,6 @@ contract DAOVoting {
         emit MemberRemoved(_member);
     }
 
-    /**
-     * @dev Get proposal details
-     * @param _proposalId The ID of the proposal
-     * @return ProposalInfo structure with proposal details
-     */
     function getProposal(uint256 _proposalId) external view returns (ProposalInfo memory) {
         Proposal storage proposal = proposals[_proposalId];
         return ProposalInfo({
@@ -152,5 +124,62 @@ contract DAOVoting {
             deadline: proposal.deadline,
             executed: proposal.executed
         });
+    }
+
+    // ✅ New Functions Below
+
+    function changeVotingPeriod(uint256 _newPeriod) external onlyAdmin {
+        require(_newPeriod > 0, "Voting period must be positive");
+        votingPeriod = _newPeriod;
+        emit VotingPeriodChanged(_newPeriod);
+    }
+
+    function hasUserVoted(uint256 _proposalId, address _user) external view returns (bool) {
+        return proposals[_proposalId].hasVoted[_user];
+    }
+
+    function getActiveProposals() external view returns (ProposalInfo[] memory) {
+        uint256 activeCount = 0;
+        for (uint256 i = 1; i <= proposalCount; i++) {
+            if (block.timestamp < proposals[i].deadline && !proposals[i].executed) {
+                activeCount++;
+            }
+        }
+
+        ProposalInfo[] memory activeProposals = new ProposalInfo[](activeCount);
+        uint256 index = 0;
+        for (uint256 i = 1; i <= proposalCount; i++) {
+            if (block.timestamp < proposals[i].deadline && !proposals[i].executed) {
+                Proposal storage p = proposals[i];
+                activeProposals[index] = ProposalInfo({
+                    id: p.id,
+                    description: p.description,
+                    forVotes: p.forVotes,
+                    againstVotes: p.againstVotes,
+                    deadline: p.deadline,
+                    executed: p.executed
+                });
+                index++;
+            }
+        }
+        return activeProposals;
+    }
+
+    function getMemberCount() external view returns (uint256) {
+        return memberCount;
+    }
+
+    function resetProposal(uint256 _proposalId) external onlyAdmin {
+        Proposal storage proposal = proposals[_proposalId];
+        require(!proposal.executed, "Cannot reset executed proposal");
+
+        proposal.forVotes = 0;
+        proposal.againstVotes = 0;
+        proposal.deadline = block.timestamp + votingPeriod;
+
+        // Reset all votes (insecure in production, just for example/testing)
+        for (uint256 i = 0; i < memberCount; i++) {
+            proposal.hasVoted[address(uint160(i))] = false;
+        }
     }
 }
